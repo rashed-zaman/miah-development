e46a1803330b262fea86cff44b1f59c759cef72b
akram-hossen-git <142209437+akram-hossen-git@users.noreply.github.com>
2024-10-15 14:12:44 +0600

    config and middleware changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,30 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "www.miah.shop" }], // Redirect www to non-www
+        destination: "https://miah.shop/:1",
+        permanent: true,
+      },
+      {
+        source: "/(.*)",
+        has: [{ type: "protocol", value: "http" }], // Redirect http to https
+        destination: "https://miah.shop/:1",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
-    domains: ['images.miah.shop'],
+    domains: ["images.miah.shop"],
   },
   env: {
-    NEXTAUTH_URL: 'http://192.168.1.119',
-    NEXT_SHARP_PATH: "/tmp/node_modules/sharp"
+    NEXTAUTH_URL: "http://192.168.1.119",
+    NEXT_SHARP_PATH: "/tmp/node_modules/sharp",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
